e06c0b6b64128b0ec165d6b796ef5ef3cadad915
gary <garyhung0584@gmail.com>
2024-10-31 15:12:37 +0800

    formatting hw20

--- hw6/hw20.py
+++ hw6/hw20.py
@@ -1,20 +1,23 @@
 def myPrint(n, m):
-    if m < -1 or m >2:
+    if m < -1 or m > 2:
         print("ERROR")
         return
+
     for i in range(n):
         for j in range(n):
             if m == 0:
-                print(f'{n*i+j:-3d}',end="")
+                print(f"{n * i + j:-3d}", end="")
             elif m == 1:
-                print(f'{n*(n-i-1)+j:-3d}',end="")
+                print(f"{n * (n - i - 1) + j:-3d}", end="")
             else:
-                print(f'{n*(n-j-1)+i:-3d}',end="")
+                print(f"{n * (n - j - 1) + i:-3d}", end="")
+
         print()
+
 
 if __name__ == "__main__":
     n = int(input())
-    if n < 1  or n > 10:
+    if n < 1 or n > 10:
         print("ERROR")
         exit()
     m = int(input())
