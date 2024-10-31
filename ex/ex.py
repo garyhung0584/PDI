96a5ca1c174f5f2d0ea84a8d9c317d053b6860c2
gary <garyhung0584@gmail.com>
2024-10-31 14:37:04 +0800

    add folder hw6, file hw19,20

---
+++ ex/ex.py
@@ -0,0 +1,5 @@
+
+n = 4
+for i in range(9):
+    print(f'({(i//3)-1+n},{(i%3)-1+n})', end=" ")
+    print()
---
+++ hw6/hw20.py
@@ -0,0 +1,27 @@
+def myPrint(n, m):
+    if m < -1 or m >2:
+        print("ERROR")
+        return
+    for i in range(n):
+        for j in range(n):
+            if m == 0:
+                print(f'{n*i+j:-3d}',end="")
+            elif m == 1:
+                print(f'{n*(n-i-1)+j:-3d}',end="")
+            else:
+                print(f'{n*(n-j-1)+i:-3d}',end="")
+        print()
+
+if __name__ == "__main__":
+    n = int(input())
+    if n < 1  or n > 10:
+        print("ERROR")
+        exit()
+    m = int(input())
+    mlist = []
+    while m != -1:
+        mlist.append(m)
+        m = int(input())
+
+    for i in mlist:
+        myPrint(n, i)
